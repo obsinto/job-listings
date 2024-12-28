ea69d0a4fcc33373570daba0a212b199bce3ee6b
Deyvid Sant <deyvidsant@hotmail.com>
2024-12-28 16:58:15 -0300

    job email fix

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
 use App\Models\Job;
-use Illuminate\Support\Facades\Auth;
+use Mail;
 
 class JobController extends Controller
 {
@@ -36,11 +37,13 @@ class JobController extends Controller
             'salary' => 'required'
         ]);
 
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
-            'employer_id' => Auth::user()->id,
+            'employer_id' => 1,
         ]);
+
+        Mail::to($job->employer->user)->send(new JobPosted($job));
         return redirect('/jobs');
     }
 
--- resources/views/mail/job-posted.blade.php
+++ resources/views/mail/job-posted.blade.php
@@ -1,12 +1,10 @@
 <x-mail::message>
-# Introduction
+    Your job "{{ $job->title }}" has posted!
 
-The body of your message.
+    <x-mail::button :url="url('/jobs/' . $job->id)">
+        View your job here!
+    </x-mail::button>
 
-<x-mail::button :url="''">
-Button Text
-</x-mail::button>
-
-Thanks,<br>
-{{ config('app.name') }}
+    Thanks,<br>
+    {{ config('app.name') }}
 </x-mail::message>
