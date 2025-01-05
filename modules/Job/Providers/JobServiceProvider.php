c1eb3a9c5b9fc43d8d2d0a81b511210129aadfcd
Deyvid Sant <deyvidsant@hotmail.com>
2025-01-05 00:27:10 -0300

    Add policies functionality

--- modules/Job/Providers/JobServiceProvider.php
+++ modules/Job/Providers/JobServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace Modules\Job\Providers;
 
-class JobServiceProvider
-{
+use Carbon\Laravel\ServiceProvider;
 
+class JobServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->mergeConfigFrom(__DIR__ . '/../config.php', 'job');
+    }
 }
